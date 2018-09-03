42f45233813fb5b643c72b5f4b22f39bb3f9676b
Diego Alvarado <diegoalvarado26@hotmail.com>
2018-09-03 20:06:57 -0300

    Ahora sí: mejorado ejercicio 2

---
+++ src/guia_3/Ejercicio_2/Instalaciones/Clase.java
@@ -0,0 +1,15 @@
+package guia_3.Ejercicio_2.Instalaciones;
+import guia_3.Ejercicio_2.Instalacion;
+
+public class Clase extends Pileta {
+
+    public String nombre_clase;
+    public int prof_id, sociosU;
+
+    public Clase(String nombre_clase, int prof_id, int sociosU){
+        this.nombre_clase = nombre_clase;
+        this.prof_id = prof_id;
+        this.sociosU = sociosU;
+    }
+
+}
---
+++ src/guia_3/Ejercicio_2/Personas/Empleados/Profesor.java
@@ -0,0 +1,22 @@
+package guia_3.Ejercicio_2.Personas.Empleados;
+
+import guia_3.Ejercicio_2.Personas.Empleado;
+
+public class Profesor extends Empleado {
+
+    int profesor_id;
+    String apellido_profesor, nombre_profesor;
+
+    public void set_tipo_empleado(){
+        tipo_empleado = this.getClass().getSimpleName();
+    }
+
+    public String get_tipo_empleado(){
+        return tipo_empleado;
+    }
+
+    @Override
+    public void setPersona(int id, String apellido, String nombre, String tipo_empleado) {
+        super.setPersona(profesor_id, apellido_profesor, nombre_profesor, get_tipo_empleado());
+    }
+}
