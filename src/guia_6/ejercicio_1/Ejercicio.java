e0d05940f254990eefe8ffe86635df3c8897328f
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-09-15 18:07:09 -0300

    -guia 6 ejercicio 1

---
+++ src/guia_6/ejercicio_1/Ejercicio.java
@@ -0,0 +1,22 @@
+package guia_6.ejercicio_1;
+
+
+
+/*
+1.1)
+1-checked
+2- unchecked
+3- checked
+4- checked
+5- unchecked
+6- checked
+ */
+
+//1.2
+public class Ejercicio extends NullPointerException {
+
+	//Debe unchekced
+	public Ejercicio() {
+		super("Value not found.");
+	}
+}
